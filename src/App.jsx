ff934ed866f7ee698edf482465851c7597015c1e
Januar202214429 <111202214429@mhs.dinus.ac.idgit>
2024-11-08 07:20:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signUp";
+import ErrorRoute from "./pages/errorRoute";
+import ForgotPassword from "./pages/forgotPassword";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance";
+
+
+
+
+const App = () => {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage/> ,
+      errorElement: <ErrorRoute/ >
+    },
+    {
+      path: "/login",
+      element: <SignInPage/>,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage/>,
+    },
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword/>
+    },
+    {
+      path: "/balance",
+      element: <BalancePage/>,
+    }
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/forgotPassword.jsx
@@ -0,0 +1,12 @@
+import FormForgotPassword from "../components/Fragments/FormForgotPassword";
+import AuthLayout from "../components/Layouts/AuthLayout";
+
+const ForgotPassword = () => {
+  return(
+    <AuthLayout type="forgot-password">
+      <FormForgotPassword/>
+    </AuthLayout>
+  );
+};
+
+export default ForgotPassword;
